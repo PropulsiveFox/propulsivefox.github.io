39ca096e583ee2a79318d98231f17330910c6534
PropulsiveFox <propulsivefox@gmail.com>
2022-03-01 23:40:04 +0900

    Restructure

---
+++ static/scripts/home/getProjects.js
@@ -0,0 +1,37 @@
+function getContents() {
+	return new Promise((resolve, reject) => {
+		fetch('https://api.github.com/repos/PropulsiveFox/propulsivefox.github.io/contents/')
+			.then(response => response.json())
+			.then(contents => resolve(contents))
+			.catch(err => reject(err));
+	});
+}
+
+function getDirsExclude() {
+	return new Promise((resolve, reject) => {
+		fetch('directories.exclude.json')
+			.then(response => response.json())
+			.then(dirsExclude => resolve(dirsExclude))
+			.catch(err => reject(err));
+	});
+}
+
+function getProjects() {
+	return Promise.all([getContents(), getDirsExclude()])
+		.then(a => {
+			a.forEach(elem => {
+				if (!(elem instanceof Array)) throw new TypeError(elem + ' is not an Array');
+			});
+
+			let contents = a[0];
+			let dirsExclude = a[1];
+
+			return contents.filter(content => {
+				if (content.type == 'dir') {
+					return !dirsExclude.includes(content.name);
+				} else return false;
+			});
+		}).catch(err => {
+			throw err;
+		});
+}
